23d0e232a47959d5faef63bcd70a448e9c9841c7
Dejan Dukic <dejan.djukic@martza-tech.com>
2023-07-21 13:39:11 +0200

    Second commit

--- src/components/CampaignList.tsx
+++ src/components/CampaignList.tsx
@@ -27,17 +27,6 @@ const CampaignList: React.FC<CampaignListProps> = ({ campaigns }) => {
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
-
-/*   const handleStartDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setStartDate(event.target.value);
-  };
-
-  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEndDate(event.target.value);
-  }; */
-  
-  
-
   const filterCampaigns = (campaign: Campaign) => {
     const campaignStartDate = new Date(campaign.startDate);
     const campaignEndDate = new Date(campaign.endDate);
@@ -67,21 +56,6 @@ const CampaignList: React.FC<CampaignListProps> = ({ campaigns }) => {
               onChange={handleSearchChange}
           /> <FontAwesomeIcon icon={faSearch} />
         </div>
-       {/*  <input
-            className='filter-data'
-            type="date"
-            placeholder="Start Date"
-            value={startDate}
-            onChange={handleStartDateChange}
-        />
-        <input
-            className='filter-data'
-            type="date"
-            placeholder="End Date"
-            value={endDate}
-            onChange={handleEndDateChange}
-            disabled={startDate === ''}
-        /> */}
         <DatePicker
             className='filter-data'
             dateFormat="yyyy/MM/dd"
@@ -115,24 +89,19 @@ const CampaignList: React.FC<CampaignListProps> = ({ campaigns }) => {
               <td>{campaign.startDate}</td>
               <td>{campaign.endDate}</td>
               <td className='active-inactive'>
-                   {/* {campaign.active &&
-    new Date(campaign.startDate) <= new Date() &&
-    new Date(campaign.endDate) >= new Date()
-        ? 'Active'
-        : 'Inactive'}  */}
-        {campaign.active ? (
-        <>
-            <FontAwesomeIcon icon={faCheckCircle} className="active-icon" /> Active
-        </>
-    ) : (
-        <>
-            <FontAwesomeIcon icon={faTimesCircle} className="inactive-icon" /> Inactive
-        </>
-    )}
-              </td>
-              <td>{campaign.Budget} $</td>
-            </tr>
-          ))}
+                {campaign.active ? (
+                <>
+                    <FontAwesomeIcon icon={faCheckCircle} className="active-icon" /> Active
+                </>
+            ) : (
+                <>
+                    <FontAwesomeIcon icon={faTimesCircle} className="inactive-icon" /> Inactive
+                </>
+            )}
+                      </td>
+                      <td>{campaign.Budget} $</td>
+                    </tr>
+                  ))}
         </tbody>
       </table>
     </div>
